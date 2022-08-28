40180f109393cc774c6269e575bb64529040dcbf
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-08-28 19:18:15 +0200

    Add getUserById method

--- InnocvAPIExercise/Networking/NetworkingProvider.swift
+++ InnocvAPIExercise/Networking/NetworkingProvider.swift
@@ -11,4 +11,21 @@ import Alamofire
 final class NetworkingProvider {
 	
 	static let shared = NetworkingProvider()
+	
+	private let kBaseURL = "https://hello-world.innocv.com/api/"
+	
+	func getUserById(id: Int) {
+		
+		let url = "\(kBaseURL)User/\(id)"
+		let kStatusCode = 200...299
+		
+		AF.request(url, method: .get).validate(statusCode: kStatusCode).responseDecodable (of: User.self) { response in
+			
+			if let user = response.value {
+				print(user)
+			} else {
+				print(response.error?.responseCode ?? "No error")
+			}
+		}
+	}
 }
