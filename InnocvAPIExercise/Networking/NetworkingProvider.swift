61f7a5853c1b60892649f6f21c8dec1e8d18dfc7
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-08-28 19:00:12 +0200

    Create NetworkingProvider

---
+++ InnocvAPIExercise/Networking/NetworkingProvider.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkingProvider.swift
+//  InnocvAPIExercise
+//
+//  Created by Rick Larios on 28/8/22.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkingProvider {
+	
+	static let shared = NetworkingProvider()
+}
