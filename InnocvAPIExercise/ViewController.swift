782254d426a6169b86e03a5ec9efda9939b9851a
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-08-28 18:51:58 +0200

    Initial Commit

---
+++ InnocvAPIExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InnocvAPIExercise
+//
+//  Created by Rick Larios on 28/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
