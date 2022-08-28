bd150484dbb807a97c1fe98badabc9107f58039e
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-08-28 19:05:08 +0200

    Create User

---
+++ InnocvAPIExercise/Networking/Data/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  InnocvAPIExercise
+//
+//  Created by Rick Larios on 28/8/22.
+//
+
+import Foundation
+
+//{
+//	"name": "Alberto2",
+//	"birthdate": "1998-06-08T00:00:00",
+//	"id": 6498
+//}
+
+struct User: Decodable {
+	
+	let name: String?
+	let birthdate: Date?
+	let id: Int?
+	
+}
